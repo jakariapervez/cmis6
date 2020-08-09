79a1d47d9da823055a211208f754caa03b7bc285
jakariapervez <jakariapervez@gmail.com>
2020-08-10 02:33:53 +0600

    Mapbox creation12

--- pac/static/pac/js/maps.js
+++ pac/static/pac/js/maps.js
@@ -14,9 +14,9 @@ var showmap= function (){
 	console.log(showStatus)
 	 map = new mapboxgl.Map({
 container: 'map',
-style: 'mapbox://styles/mapbox/streets-v11',
+style: 'mapbox://styles/mapbox/satellite-streets-v11',
 center: [91.2781751,24.39824583],
- zoom:12
+ zoom:17
 
 });	
 	showStatus=true;	
