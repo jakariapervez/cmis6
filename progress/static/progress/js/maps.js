4c0be9bc8f582c66f38e65335d242f1838a57499
jakariapervez <jakariapervez@gmail.com>
2020-08-15 21:43:27 +0600

    Update Map 3
    Update Map 3

--- progress/static/progress/js/maps.js
+++ progress/static/progress/js/maps.js
@@ -13,6 +13,7 @@ var addBoundary=function (mydata)
 	
 var showMap=function (mydata) 
 {
+//console.log(mydata.boundary)
 mapboxgl.accessToken = 'pk.eyJ1IjoiamFrYXJpYXBlcnZleiIsImEiOiJjazY0dnJ4ZmkxMGhzM29wZXUxOWw4dTV4In0.ZFnBYL5QF_8aLrddh2JqBA';	
 var map= new mapboxgl.Map({
 	
@@ -22,6 +23,7 @@ center: [ 91.27817518,24.39824583],
  zoom:17
 	
 });	
+map.addsource('boundary',{})
 }
 
 
