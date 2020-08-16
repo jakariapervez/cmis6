c23bd0b4a9313ebf9751e76ed713906afbf2db54
jakariapervez <jakariapervez@gmail.com>
2020-08-16 20:14:30 +0600

    Management fucntion for Invoice total
    Management fucntion for Invoice total

--- progress/load.py
+++ progress/load.py
@@ -108,6 +108,18 @@ def build_poly(verbose=True):
 
     #p = Polygon()
     #print(p)
+from pac.models import Invoice_details,Expenditure_details
+def getInvoiceTotal(invoice):
+    invoice_total=0
+    expenditures=Expenditure_details.objects.all().filter(Invoice_details=invoice)
+    for exp in expenditures:
+        invoice_total+=float(exp.Total)
+        print("exp_total={} invoice_total={}".format(exp.Total,invoice_total))
+def updateInvoiceTotal(verbose=True):
+    invoices=Invoice_details.objects.all().filter(FinancialYear=5)
+    for invoice in invoices:
+        print(invoice.Invoice_no)
+        getInvoiceTotal(invoice)
 
 
 def run(verbose=True):
