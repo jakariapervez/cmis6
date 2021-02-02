0afb31f8c635df90816fd10348f5df3cc1e29206
jakariapervez <jakariapervez@gmail.com>
2021-02-02 23:38:46 +0600

    Flood Damage Assesment
    Flood Damage Assesment

---
+++ Civilworks cost/ACAD TUTORIAL/ACAD_TUT_01/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+namespace ACAD_TUT_01
+{
+    public class Class1
+    {
+        [CommandMethod("cl")]
+        public static void CreateCircle()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Database db = doc.Database;
+            //start transection
+            using (Transaction trans = db.TransactionManager.StartTransaction())
+            { 
+            
+            
+            }
+        
+        }
+    }
+}
