b1682e179a41dadb5eac4227e4b39619488adb9b
jakariapervez <jakariapervez@gmail.com>
2021-02-19 15:04:31 +0600

    HH
    HH

--- wldata/admin.py
+++ wldata/admin.py
@@ -1,22 +1,27 @@
 from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
-from .models import SMS_info,GaugeLocation,GaugeReading,GaugeAccessibility,DivisionNames
-# Register your models here.
+from .models import SMS_info,DivisionNames,River,GaugeReader,GaugeLocation,GaugeReading,ReportedGauge,communicationList# Register your models here.
 @admin.register(SMS_info)
 class SMS_info_Admin(ImportExportModelAdmin):
     pass
-
-
-
+@admin.register(DivisionNames)
+class DivisionNames_Admin(ImportExportModelAdmin):
+    pass
+@admin.register(River)
+class GaugeAccessibility_Admin(ImportExportModelAdmin):
+    pass
+@admin.register(GaugeReader)
+class GaugeAccessibility_Admin(ImportExportModelAdmin):
+    pass
 @admin.register(GaugeLocation)
 class GaugeLocation_Admin(ImportExportModelAdmin):
     pass
 @admin.register(GaugeReading)
 class GaugeReading_Admin(ImportExportModelAdmin):
     pass
-@admin.register(DivisionNames)
-class DivisionNames_Admin(ImportExportModelAdmin):
+@admin.register(ReportedGauge)
+class GaugeReading_Admin(ImportExportModelAdmin):
     pass
-@admin.register(GaugeAccessibility)
-class GaugeAccessibility_Admin(ImportExportModelAdmin):
+@admin.register(communicationList)
+class GaugeReading_Admin(ImportExportModelAdmin):
     pass
--- wldata/models.py
+++ wldata/models.py
@@ -14,21 +14,42 @@ class SMS_info(models.Model):
 
     def __str__(self):
         return str(self.mobile_no)+":"+self.smsbody+":"+self.mobile_no
-class GaugeLocation(models.Model):
-    gauge_name=models.CharField(max_length=50,null=True,blank=True)
-    river_name=models.CharField(max_length=100,null=True,blank=True)
-    latitude=models.DecimalField(max_digits=10,decimal_places=6)
-    longitude= models.DecimalField(max_digits=10, decimal_places=6)
 
-class DivisionNames(models.Model):
-    division_name=models.CharField(max_length=300,null=True,blank=True)
-    division_short_code=models.CharField(max_length=40,null=True,blank=True)
 
+
+"""  
 class GaugeAccessibility(models.Model):
     gid=models.ForeignKey(GaugeLocation,on_delete=models.CASCADE, null=True, blank=True)
     divid=models.ForeignKey(DivisionNames,on_delete=models.CASCADE, null=True, blank=True)
+ """
+class DivisionNames(models.Model):
+    division_name=models.CharField(max_length=300,null=True,blank=True)
+    division_short_code=models.CharField(max_length=40,null=True,blank=True)
+class River(models.Model):
+    river_no=models.CharField(max_length=30,null=True,blank=True)
+    river_name=models.CharField(max_length=200,null=True,blank=True)
+class GaugeReader(models.Model):
+    reader_name=models.CharField(max_length=200,null=True,blank=True)
+    mobile_no=models.CharField(max_length=25,null=True,blank=True)
+class GaugeLocation(models.Model):
+    gauge_code=models.CharField(max_length=25,null=True,blank=True)
+    latitude=models.DecimalField(max_digits=10,decimal_places=6)
+    longitude= models.DecimalField(max_digits=10, decimal_places=6)
+    reader=models.ForeignKey(GaugeReader,on_delete=models.CASCADE, null=True, blank=True)
+    river_name=models.ForeignKey(River,on_delete=models.CASCADE, null=True, blank=True)
+    division_name=models.ForeignKey(DivisionNames,on_delete=models.CASCADE, null=True, blank=True)
+
 class GaugeReading(models.Model):
     gauge_name=models.ForeignKey(GaugeLocation,on_delete=models.CASCADE, null=True, blank=True)
     datetime=models.DateTimeField(auto_now=True)
     wlreading=models.DecimalField(max_digits=10,decimal_places=3)
+class ReportedGauge(models.Model):
+    gauge_name=models.ForeignKey(GaugeLocation,on_delete=models.CASCADE,null=True,blank=True)
+    division_name=models.ForeignKey(DivisionNames,on_delete=models.CASCADE,null=True,blank=True)
+class communicationList(models.Model):
+    person_designation = models.CharField(max_length=200, null=True, blank=True)
+    person_email = models.CharField(max_length=200, null=True, blank=True)
+    person_mobile= models.CharField(max_length=200, null=True, blank=True)
+
+
 
