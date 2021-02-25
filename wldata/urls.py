2478875becfe766d0cea5761c155184201846d7c
jakariapervez <jakariapervez@gmail.com>
2021-02-25 07:30:23 +0600

    Banda soft u2
    Banda soft u2

--- wldata/urls.py
+++ wldata/urls.py
@@ -4,7 +4,11 @@ from . import views
 urlpatterns=[
     url(r'^$',views.wl_index,name='wl_index'),
     url(r'^data_collect',views.data_collect_view,name='wl_data_collect'),
-    url(r'^view_data',views.displayData,name='wl_data_display')
+    url(r'^view_data',views.displayData,name='wl_data_display'),
+    url(r'^send_email',views.sendEmail,name='send_email'),
+    url(r'^wl_logout',views.wl_Logiut,name='wl_logout'),
+
+
 
 
 
--- wldata/views.py
+++ wldata/views.py
@@ -4,18 +4,22 @@ from django.shortcuts import redirect
 from .models import SMS_info
 # Create your views here.
 def wl_index(request):
-    dppitems = SMS_info.objects.all()
-    return render(request, 'wldata/accounts_dashboard2.html', {'dppitems': dppitems})
+    sms_items= SMS_info.objects.all()
+    return render(request, 'wldata/accounts_dashboard2.html', {'sms_items': sms_items})
 def data_collect_view(request):
     if request.method == 'GET':
         tbody = request.GET['text']
         cellno = request.GET['number']
         mysms=SMS_info(smsbody=tbody,mobile_no=cellno)
         mysms.save()
-    dppitems = SMS_info.objects.all()
-    return render(request, 'wldata/accounts_dashboard2.html', {'dppitems': dppitems})
+    sms_items = SMS_info.objects.all()
+    return render(request, 'wldata/accounts_dashboard2.html', {'sms_items':sms_items})
 
     #return redirect('wl_index')
 def displayData(request):
+    return redirect(data_collect_view)
+def sendEmail(request):
+    return redirect(data_collect_view)
+def wl_Logiut(request):
     return redirect(data_collect_view)
 
