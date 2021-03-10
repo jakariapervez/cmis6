c0f4ecff36c43f27ef1c08a7b291fc8b2df001ea
jakariapervez <jakariapervez@gmail.com>
2021-03-10 14:55:08 +0600

    Livelihood 16-17 update 12
    Livelihood 16-17 update 12

--- pac/load.py
+++ pac/load.py
@@ -1,2 +1,12 @@
 import os
-print(os.path.realpath(__file__))
+import pandas as pd
+def disPlayDF(mydf):
+    for index,row in mydf.iterrows():
+        print(row)
+
+def createLivelihoodExpenditure(verbose=True):
+    myfolder=os.path.abspath(os.path.dirname(__file__))
+    print(myfolder)
+    exp_input_path=os.path.join('LH.csv')
+    input_df=pd.read_csv(exp_input_path)
+    disPlayDF(input_df)
